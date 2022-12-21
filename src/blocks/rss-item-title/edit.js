9655609c17d1fae8450d41390d8f3c4db08af788
Cory Hughart <cory@blackbird.digital>
2022-12-21 15:39:30 -0500

    rss-item-title: add heading level, text align, and link controls

--- src/blocks/rss-item-title/edit.js
+++ src/blocks/rss-item-title/edit.js
@@ -25,6 +25,84 @@ import HeadingLevelDropdown from './heading-level-dropdown';
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit( { attributes, context: { title } } ) {
-	return <h2 { ...useBlockProps() }>{ title }</h2>;
+export default function Edit( {
+	attributes: { level, textAlign, isLink, rel, linkTarget },
+	setAttributes,
+	context: { title, url },
+} ) {
+	const Tag = 0 === level ? 'p' : `h${ level }`;
+	const blockProps = useBlockProps( {
+		className: classnames( {
+			[ `has-text-align-${ textAlign }` ]: textAlign,
+		} ),
+	} );
+	const titleContent = title && title !== '' ? title : __( 'RSS Item Title' );
+	let titleElement = <Tag { ...blockProps }>{ titleContent }</Tag>;
+	if ( isLink && url ) {
+		titleElement = (
+			<Tag { ...blockProps }>
+				<a
+					href={ url }
+					rel={ rel }
+					target={ linkTarget }
+					onClick={ ( event ) => event.preventDefault() }
+				>
+					{ titleContent }
+				</a>
+			</Tag>
+		);
+	}
+
+	return (
+		<>
+			<BlockControls group="block">
+				<HeadingLevelDropdown
+					selectedLevel={ level }
+					onChange={ ( newLevel ) =>
+						setAttributes( { level: newLevel } )
+					}
+				/>
+				<AlignmentControl
+					value={ textAlign }
+					onChange={ ( nextAlign ) => {
+						setAttributes( { textAlign: nextAlign } );
+					} }
+				/>
+			</BlockControls>
+			<InspectorControls>
+				<PanelBody title={ __( 'Link Settings' ) }>
+					<ToggleControl
+						label={ __( 'Make title a link' ) }
+						checked={ isLink }
+						onChange={ ( nextIsLink ) => {
+							setAttributes( { isLink: nextIsLink } );
+						} }
+					/>
+					{ isLink && (
+						<>
+							<ToggleControl
+								label={ __( 'Open in new tab' ) }
+								checked={ linkTarget === '_blank' }
+								onChange={ ( nextIsNewTab ) => {
+									setAttributes( {
+										linkTarget: nextIsNewTab
+											? '_blank'
+											: '_self',
+									} );
+								} }
+							/>
+							<TextControl
+								label={ __( 'Link rel' ) }
+								value={ rel }
+								onChange={ ( nextRel ) => {
+									setAttributes( { rel: nextRel } );
+								} }
+							/>
+						</>
+					) }
+				</PanelBody>
+			</InspectorControls>
+			{ titleElement }
+		</>
+	);
 }
