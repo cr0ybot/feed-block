1c54d5eb051d934ac8a5b9d5524bcb8d064a3fc0
Cory Hughart <cory@blackbird.digital>
2022-12-21 17:46:51 -0500

    rss-item-template server render

--- src/blocks/rss-item-template/render.php
+++ src/blocks/rss-item-template/render.php
@@ -2,10 +2,60 @@
 /**
  * Block: rss-item-template, render.
  *
+ * Global vars: $attributes, $content, $block.
+ *
  * @package rss-block
  */
+
+use function RSSBlock\Feed\get_feed;
+
+$feed = get_feed( $block->context['feedURL'] );
+
+if ( is_wp_error( $feed ) ) {
+	if ( WP_DEBUG ) {
+		error_log( 'RSS Block: ' . $feed->get_error_message() . " ({$block->context['feedURL']})" ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+	}
+	return;
+}
+
+$classnames = '';
+if ( isset( $block->context['displayLayout'] ) ) {
+	if ( isset( $block->context['displayLayout']['type'] ) && 'flex' === $block->context['displayLayout']['type'] ) {
+		$classnames = "is-flex-container columns-{$block->context['displayLayout']['columns']}";
+	}
+}
+
+$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classnames ) );
 ?>
 
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Rss Item Template – hello from a dynamic block!', 'rss-block' ); ?>
-</p>
+<ul <?php echo $wrapper_attributes; ?>>
+<?php
+for ( $i = 0; $i < $block->context['itemsToShow']; $i++ ) :
+	$item = $feed['items'][ $i ];
+
+	// Get an instance of the current RSS Item Template block.
+	$block_instance = $block->parsed_block;
+
+	// Set the block name to one that does not correspond to an existing registered block.
+	// This ensures that for the inner instances of the RSS Item Template block, we do not render any block supports.
+	$block_instance['blockName'] = 'core/null';
+
+	// Render the inner blocks of the RSS Item Template block with `dynamic` set to `false` to prevent calling
+	// `render_callback` and ensure that no wrapper markup is included.
+	$block_content = (
+		new WP_Block(
+			$block_instance,
+			$item,
+		)
+	)->render( array( 'dynamic' => false ) );
+
+	// Wrap the render inner blocks in a `li` element with the appropriate post classes.
+	$item_classes = 'wp-block-rss-block-rss-item';
+	?>
+	<li class="<?php echo esc_attr( $item_classes ); ?>">
+		<?php echo $block_content; ?>
+	</li>
+	<?php
+endfor;
+?>
+</ul>
