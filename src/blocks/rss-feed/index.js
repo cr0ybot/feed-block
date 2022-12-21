2583c0ad6dd92215156b0569fa1bd3c62bbb8c9e
Cory Hughart <cory@blackbird.digital>
2022-12-21 17:46:24 -0500

    cleanup

--- src/blocks/rss-feed/index.js
+++ src/blocks/rss-feed/index.js
@@ -7,18 +7,10 @@ import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
 import './editor.scss';
 
-/**
- * Internal dependencies
- */
+import metadata from './block.json';
 import edit from './edit';
 import save from './save';
-import metadata from './block.json';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
 registerBlockType( metadata, {
 	/**
 	 * @see ./edit.js
--- src/blocks/rss-item-title/index.js
+++ src/blocks/rss-item-title/index.js
@@ -9,21 +9,13 @@ import { title as icon } from '@wordpress/icons';
 import './style.scss';
 import './editor.scss';
 
-/**
- * Internal dependencies
- */
-import Edit from './edit';
 import metadata from './block.json';
+import edit from './edit';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
 registerBlockType( metadata.name, {
 	icon,
 	/**
 	 * @see ./edit.js
 	 */
-	edit: Edit,
+	edit,
 } );
