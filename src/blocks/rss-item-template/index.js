a143aa8ff4e781f4fa26c5a7290e05573d7bb4cc
Cory Hughart <cory@blackbird.digital>
2022-12-21 17:46:14 -0500

    add save to rss-item-template

--- src/blocks/rss-item-template/index.js
+++ src/blocks/rss-item-template/index.js
@@ -9,21 +9,18 @@ import { layout as icon } from '@wordpress/icons';
 import './style.scss';
 import './editor.scss';
 
-/**
- * Internal dependencies
- */
-import Edit from './edit';
 import metadata from './block.json';
+import edit from './edit';
+import save from './save';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
 registerBlockType( metadata.name, {
 	icon,
 	/**
 	 * @see ./edit.js
 	 */
-	edit: Edit,
+	edit,
+	/**
+	 * @see ./save.js
+	 */
+	save,
 } );
---
+++ src/blocks/rss-item-template/save.js
@@ -0,0 +1,9 @@
+/**
+ * Block: rss-item-template, save.
+ */
+
+import { InnerBlocks } from '@wordpress/block-editor';
+
+export default function Save() {
+	return <InnerBlocks.Content />;
+}
