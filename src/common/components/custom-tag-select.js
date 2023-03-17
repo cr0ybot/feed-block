37bd06d543a76119be73d87c0037c21208721463
Cory Hughart <cr0ybot@gmail.com>
2023-03-17 14:21:30 -0400

    include custom content tag description

--- src/common/components/custom-tag-select.js
+++ src/common/components/custom-tag-select.js
@@ -7,30 +7,36 @@ import { __ } from '@wordpress/i18n';
 
 export function CustomTagSelect( { custom, onChange, selected } ) {
 	return (
-		<TreeSelect
-			label={ __( 'Custom Content Tag', 'feed-block' ) }
-			description={ __(
-				'Select a custom tag to display instead of the default.',
-				'feed-block'
-			) }
-			selectedId={ selected.join( '|' ) }
-			noOptionLabel={ __( 'Select a custom tag', 'feed-block' ) }
-			tree={ Object.entries( custom ).map( ( [ namespace, tags ] ) => ( {
-				name: namespace,
-				id: '',
-				children: Object.entries( tags ).map( ( [ tag ] ) => ( {
-					name: tag,
-					id: `${ namespace }|${ tag }`,
-				} ) ),
-			} ) ) }
-			onChange={ ( nextTag ) => {
-				if ( nextTag === '' ) {
-					onChange( [] );
-				} else {
-					onChange( nextTag.split( '|' ) );
-				}
-			} }
-		/>
+		<>
+			<p className="description">
+				{ __(
+					'Select a custom feed tag to display instead of the default.',
+					'feed-block'
+				) }
+			</p>
+			<TreeSelect
+				label={ __( 'Custom Content Tag', 'feed-block' ) }
+				selectedId={ selected.join( '|' ) }
+				noOptionLabel={ __( 'Use default content', 'feed-block' ) }
+				tree={ Object.entries( custom ).map(
+					( [ namespace, tags ] ) => ( {
+						name: namespace,
+						id: '',
+						children: Object.entries( tags ).map( ( [ tag ] ) => ( {
+							name: tag,
+							id: `${ namespace }|${ tag }`,
+						} ) ),
+					} )
+				) }
+				onChange={ ( nextTag ) => {
+					if ( nextTag === '' ) {
+						onChange( [] );
+					} else {
+						onChange( nextTag.split( '|' ) );
+					}
+				} }
+			/>
+		</>
 	);
 }
 
